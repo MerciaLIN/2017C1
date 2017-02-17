97e1cba0ceb5ebfc1060207d6aa6e3a5d6c812c1
Ivy Liao <Ivy Liao@DESKTOP-KODUC56>
2017-02-17 17:31:52 +0800

    修改過

--- SimpleCalculator02/Form1.cs
+++ SimpleCalculator02/Form1.cs
@@ -16,5 +16,71 @@ namespace SimpleCalculator02
         {
             InitializeComponent();
         }
+
+
+
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Calculate(1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Calculate(2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Calculate(3);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Calculate(4);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calculate(int i)
+
+        {
+            int x = int.Parse(textBox1.Text);
+            int y = int.Parse(textBox2.Text);
+            switch (i)
+            {
+                case 1:
+                    label1.Text = (x + y).ToString();
+                    break;
+                case 2:
+                    label1.Text = (x - y).ToString();
+                    break;
+                case 3:
+                    label1.Text = (x * y).ToString();
+                    break;
+                case 4:
+                    label1.Text = (x / y).ToString();
+                    break;
+            }
+        }
     }
 }
